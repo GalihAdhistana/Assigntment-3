e0b30c569bd7c2802ecb3ceddc6c5dda3dd81b70
GalihAdhistana <76497965+GalihAdhistana@users.noreply.github.com>
2021-01-12 23:10:12 +0100

    Add files via upload

---
+++ Assignment 3/Assignment 3/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Assignment_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b, c;
+
+            Console.Write("Please enter your number : ");
+            a = int.Parse(Console.ReadLine());
+
+            int[] array = new int[a + 1];
+            for (b = 2; b <= a; b++)
+            {
+                array[b] = 1;
+            }
+            for (b = 2; b <= a; b++)
+            {
+                if (array[b] == 1)
+                {
+                    for (c = 2; c * b <= a; c++)
+                    {
+                        array[c * b] = 0;
+                    }
+                }
+            }
+            Console.Write("The prime numbers are : ");
+            for (b = 2; b <= a; b++)
+            {
+                if (array[b] == 1)
+                {
+                    Console.Write(b +" ");
+                }
+            }
+        }
+    }
+}
